d2650205abc2036169dc7aafcccaa30599101334
Phinatech <ugbojudithchinemerem@gmail.com>
2023-02-06 23:22:36 +0100

    one to many relationship(authentication)

--- validation/validation.ts
+++ validation/validation.ts
@@ -4,11 +4,12 @@ import {NextFunction} from "express"
 
 //Central validation Function
 const validator = async (
-    schemaName: Joi.ObjectSchema,
+    schemaName: Joi.ObjectSchema, //It expecting a object Schema for it and setting an interface or it.
     body:object,
     next:NextFunction
     ): Promise<void> => {
         const value = await schemaName.validate(body,{
+            //These below help you to caputure some errors
             abortEarly:false,// it prints or caputure all the error
             allowUnknown:true,
             stripUnknown:true,
